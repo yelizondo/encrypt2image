c707ff80d899663f0ca0eca6672ee8ee241e527c
yelizondo <yubeg971@gmail.com>
2017-12-15 15:08:57 -0600

    Building menu

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,31 @@
+import java.util.Scanner;
+
 public class Main
 {
+    private Scanner Reader;
+
+    public Main()
+    {
+        Reader = new Scanner(System.in);
+    }
+
+    private void menu()
+    {
+        System.out.println();
+        System.out.print("Write the name of the file you want to read > ");
+        
+        String input = this.Reader.nextLine();
+        Reader.close();
+
+        System.out.println(input);
+    }
 
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        Main main = new Main();
+        main.menu();
     }
 }
+    ;  // Reading from System.in
+
+
